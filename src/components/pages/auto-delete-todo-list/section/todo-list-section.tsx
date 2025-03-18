a21cc2a745c6a99da676414d700f01200b023ecb
phurin <le.phurin.work@gmail.com>
2025-03-19 00:40:45 +0700

    feat: improve auto delete

--- src/components/pages/auto-delete-todo-list/section/todo-list-section.tsx
+++ src/components/pages/auto-delete-todo-list/section/todo-list-section.tsx
@@ -22,10 +22,25 @@ export const TodoListSection: FC<ITodoListSectionProps> = ({ data }) => {
       prevState.filter((item) => item.name !== product.name)
     );
 
-    if (product.type === "Fruit")
+    if (product.type === "Fruit") {
       setFruits((prevState) => [...prevState, product]);
-    if (product.type === "Vegetable")
+      setTimeout(() => {
+        setFruits((prevState) =>
+          prevState.filter((item) => item.name !== product.name)
+        );
+        setProducts((prevProducts) => [...prevProducts, product]);
+      }, timeout);
+    }
+
+    if (product.type === "Vegetable") {
       setVegetables((prevState) => [...prevState, product]);
+      setTimeout(() => {
+        setVegetables((prevState) =>
+          prevState.filter((item) => item.name !== product.name)
+        );
+        setProducts((prevProducts) => [...prevProducts, product]);
+      }, timeout);
+    }
   };
 
   const handleClickProductUnavailable = (product: IProducts) => {
@@ -40,36 +55,6 @@ export const TodoListSection: FC<ITodoListSectionProps> = ({ data }) => {
         prevState.filter((item) => item.name !== product.name)
       );
   };
-
-  const moveFirstItemBack = (
-    setCategory: React.Dispatch<React.SetStateAction<IProducts[]>>,
-    category: IProducts[]
-  ) => {
-    if (category.length === 0) return;
-
-    const firstItem = category[0];
-
-    setProducts((prevProducts) => [...prevProducts, firstItem]);
-    setCategory((prevCategory) => prevCategory.slice(1));
-  };
-
-  useEffect(() => {
-    if (fruits?.length) {
-      const timer = setTimeout(() => {
-        moveFirstItemBack(setFruits, fruits);
-      }, 5000);
-      return () => clearTimeout(timer);
-    }
-  }, [fruits.length]);
-
-  useEffect(() => {
-    if (vegetables?.length) {
-      const timer = setTimeout(() => {
-        moveFirstItemBack(setVegetables, vegetables);
-      }, 5000);
-      return () => clearTimeout(timer);
-    }
-  }, [vegetables.length]);
 
   return (
     <div className="w-full h-full flex justify-between gap-x-8">
