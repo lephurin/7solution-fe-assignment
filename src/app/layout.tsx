66d4ad74b0c098bca71bbe62c27506495facf71f
phurin <le.phurin.work@gmail.com>
2025-03-19 21:04:00 +0700

    feat: update package json

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,12 +25,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Providers>{children}</Providers>
-      </body>
-    </html>
+    <Providers>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
+      </html>
+    </Providers>
   );
 }
