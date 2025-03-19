274ae0e0eb9a994dfb1533bf02d09623e5663508
phurin <le.phurin.work@gmail.com>
2025-03-20 03:21:52 +0700

    update sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e  # Stop script on error
 
-echo "Ensuring correct permissions..."
-sudo chown -R ubuntu:ubuntu /home/ubuntu/my-nextjs-app
-
 echo "Installing dependencies..."
 cd /home/ubuntu/my-nextjs-app
-sudo npm install --unsafe-perm=true --allow-rootAllows running as different users
+sudo npm install --unsafe-perm=true --allow-root  # Allows running as different users
 
 echo "Dependencies installed successfully."
