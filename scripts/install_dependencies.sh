964d7e8d5cf0fde1dd7dff1ca3a6b860c83b0bc7
phurin <le.phurin.work@gmail.com>
2025-03-20 04:17:21 +0700

    update sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,6 +3,8 @@ set -e  # Stop script on error
 
 echo "Installing dependencies..."
 cd /home/ubuntu/my-nextjs-app
-npm install
+rm -rf node_modules package-lock.json
+npm cache clean --force
+npm install --no-fund --no-audit
 
 echo "Dependencies installed successfully."
