d7afaec16a1096180e18aa0942f507824ed5cf18
phurin <le.phurin.work@gmail.com>
2025-03-20 03:28:56 +0700

    update sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,9 +4,6 @@ set -e  # Stop script on error
 echo "Navigating to application directory..."
 cd /home/ubuntu/my-nextjs-app
 
-echo "Building the application..."
-npm run build
-
 echo "Starting application using PM2..."
 pm2 delete my-nextjs-app || true  # Remove old process if exists
 pm2 start npm --name "my-nextjs-app" -- start
